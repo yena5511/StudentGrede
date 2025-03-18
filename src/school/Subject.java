22e4b7f70ad7cbd770fb3250ec2ececa8ab00834
yena5511 <ar01077422264@gmail.com>
2025-03-18 10:33:39 +0900

    initial commit

---
+++ src/school/Subject.java
@@ -0,0 +1,55 @@
+package school;
+
+import utils.Define;
+
+import java.util.ArrayList;
+
+public class Subject {
+    private String subjectName; // 과목 이름
+    private int subjectId;  // 과목 고유 번호
+    private int gradeType;  // 학점 평가 정책
+
+    private ArrayList<Student> studentList = new ArrayList<Student>();
+
+    public Subject(String subjectName, int subjectId) {
+        this.subjectName = subjectName;
+        this.subjectId = subjectId;
+        this.gradeType = Define.AB_TYPE;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public int getGradeType() {
+        return gradeType;
+    }
+
+    public void setGradeType(int gradeType) {
+        this.gradeType = gradeType;
+    }
+
+    public void register(Student student) {
+        studentList.add(student);
+    }
+}
